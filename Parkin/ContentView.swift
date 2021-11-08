800715795095b3b3fb4f1331ac957291c39bf672
roroquentin <hasan.guzelmansur@icloud.com>
2021-11-08 21:35:20 +0300

    Initial Commit

---
+++ Parkin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parkin
+//
+//  Created by Hasan Güzelmansur on 8.11.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Parkin/ParkinApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkinApp.swift
+//  Parkin
+//
+//  Created by Hasan Güzelmansur on 8.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ParkinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
